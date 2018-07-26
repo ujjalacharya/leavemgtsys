a102ee7fd2984023e4d9218a0e5bc397d8eb07c5
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-07-26 19:33:05 +0545

    makes registration and login working

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const PORT      = process.env.PORT || 3000;
 const exphbs    = require('express-handlebars');
 const keys      = require('./config/keys');
 const passport  = require('passport');
+const bodyParser= require('body-parser');
+const session = require('express-session');
 
 //Connect to the database
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
@@ -18,6 +20,17 @@ require('./config/passport')(passport);
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//BodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Middlware for session and flash
+app.use(session({
+    secret: 'ujjals secret',
+    resave: false,
+    saveUninitialized: true,
+}))
+
 //import the routes
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -25,6 +38,11 @@ const users = require('./routes/users')
 //Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next();
+})
 
 //Using the routes
 app.use('/', index)
